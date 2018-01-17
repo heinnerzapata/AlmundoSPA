41e9546fbc31a556d61383b0fc97900ee807e486
Your Name <you@example.com>
2018-01-16 23:20:09 -0500

    basic components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+        {
+          path: '',
+          component: HomeComponent
+        },
+        {
+          path: 'home',
+          component: HomeComponent
+        }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
